b48b41de7d351b3746bb0658c2fca7b4006fca83
ekimaycicek <ekimaycicek@icloud.com>
2023-09-03 18:12:16 +0300

    new

---
+++ src/main/java/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+
+
+
+        private String name;
+        private String surname;
+        private int studentNumber;
+        private int year;
+
+        public Student(String name, String surname, int studentNumber, int year) {
+            this.name = name;
+            this.surname = surname;
+            this.studentNumber = studentNumber;
+            this.year = year;
+        }
+
+        public void informations() {
+            System.out.println("Name surname: " + name + " " + surname);
+            System.out.println("Student number: " + studentNumber);
+            System.out.println("Year: " + year);
+        }
+    }
+
+
+
+
+
+
