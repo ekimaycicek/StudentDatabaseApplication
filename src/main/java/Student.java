8bdbe28bbb1609fb7e7962be6cc1923025cb1291
ekimaycicek <ekimaycicek@icloud.com>
2024-01-14 22:59:43 +0300

    new

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -11,6 +11,7 @@ public class Student {
 
 
 
+
         public Student(String name, String surname, int studentNumber, int year) {
             this.name = name;
             this.surname = surname;
